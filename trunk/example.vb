2f9d4405bae95cec241d36b760818563bbb9cc39
espendiller <espendiller@d3a61da7-533b-44c3-a7de-bb04029eab85>
2009-10-10 12:18:07 +0000

    added usage example

---
+++ trunk/example.vb
@@ -0,0 +1,9 @@
+'$Id$
+Dim beispiel As New myPlotter(Inventor.Document.ActiveDocument)
+beispiel.RotatePlot.A0 = True
+beispiel.AllColorsAsBlack = True
+beispiel.NumberOfCopies = 2
+beispiel.SetSystemPrinter = "PDFCreator"
+beispiel.UpdateIFeatures = True
+beispiel.UpdatePlotstyles = True
+beispiel.plot()
